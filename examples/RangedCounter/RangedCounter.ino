70f2c0f9fec7cfa7371eca4218c3c8572b841ec1
Lennart Hennigs <mail@lennarthennigs.de>
2020-04-20 22:49:34 +0200

    v1.2.1 - bugfix

---
+++ examples/RangedCounter/RangedCounter.ino
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////
+
+#include "Button2.h"; //  https://github.com/LennartHennigs/Button2
+#include "ESPRotary.h";
+
+/////////////////////////////////////////////////////////////////
+
+#define ROTARY_PIN1	D1
+#define ROTARY_PIN2	D2
+#define BUTTON_PIN	D4
+
+#define CLICKS_PER_STEP   4   // this number depends on your rotary encoder 
+#define MIN_POS   5
+#define MAX_POS   20
+
+
+/////////////////////////////////////////////////////////////////
+
+ESPRotary r = ESPRotary(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP, MIN_POS, MAX_POS);
+Button2 b = Button2(BUTTON_PIN);
+
+/////////////////////////////////////////////////////////////////
+
+void setup() {
+  Serial.begin(9600);
+  delay(50);
+  Serial.println("\n\nRanged Counter");
+  Serial.println("You can only set values between " + String(MIN_POS) + " and " + String(MAX_POS));
+  
+  r.setChangedHandler(rotate);
+  r.setLeftRotationHandler(showDirection);
+  r.setRightRotationHandler(showDirection);
+
+  b.setTapHandler(click);
+  b.setLongClickHandler(resetPosition);
+}
+
+void loop() {
+  r.loop();
+  b.loop();
+}
+
+/////////////////////////////////////////////////////////////////
+
+// on change
+void rotate(ESPRotary& r) {
+   Serial.println(r.getPosition());
+}
+
+// on left or right rotation
+void showDirection(ESPRotary& r) {
+  Serial.println(r.directionToString(r.getDirection()));
+}
+ 
+// single click
+void click(Button2& btn) {
+  Serial.println("Click!");
+}
+
+// long click
+void resetPosition(Button2& btn) {
+  r.resetPosition();
+  Serial.println("Reset!");
+}
+
+/////////////////////////////////////////////////////////////////

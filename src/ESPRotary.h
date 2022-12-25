516e349d9ce4d8511aa48f6f974489216bfd5ffd
Lennart Hennigs <mail@lennarthennigs.de>
2022-12-25 07:31:44 +0100

    fixed formatting

--- src/ESPRotary.h
+++ src/ESPRotary.h
@@ -24,15 +24,15 @@ enum direction {
 /////////////////////////////////////////////////////////////////
 
 class ESPRotary {
- protected:
-   int id;
-   byte pin1, pin2;
-   byte steps_per_click;
-   int lower_bound, upper_bound;
-   int position, last_position;
-   direction dir;
-   byte state;
-   int increment;
+  protected:
+    int id;
+    byte pin1, pin2;
+    byte steps_per_click;
+    int lower_bound, upper_bound;
+    int position, last_position;
+    direction dir;
+    byte state;
+    int increment;
 
     typedef void (*CallbackFunction) (ESPRotary&);
     CallbackFunction change_cb = NULL;
@@ -41,47 +41,46 @@ class ESPRotary {
     CallbackFunction lower_cb = NULL;
     CallbackFunction upper_cb = NULL;
 
- public:
-   ESPRotary();
-   ESPRotary(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
+  public:
+    ESPRotary();
+    ESPRotary(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
 
-   void begin(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
+    void begin(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
 
-   int getPosition() const;
-   void resetPosition(int p = 0, bool fireCallback = true);
+    int getPosition() const;
+    void resetPosition(int p = 0, bool fireCallback = true);
 
-   direction getDirection() const;
-   String directionToString(direction dir) const;
+    direction getDirection() const;
+    String directionToString(direction dir) const;
 
-   void setIncrement(int inc);
-   int getIncrement() const;
+    void setIncrement(int inc);
+    int getIncrement() const;
 
-   void setUpperBound(int upper_bound);
-   void setLowerBound(int lower_bound);
-   int getUpperBound() const;
-   int getLowerBound() const;
+    void setUpperBound(int upper_bound);
+    void setLowerBound(int lower_bound);
+    int getUpperBound() const;
+    int getLowerBound() const;
 
-   void setStepsPerClick(int steps);
-   int getStepsPerClick() const;
+    void setStepsPerClick(int steps);
+    int getStepsPerClick() const;
    
-   void setChangedHandler(CallbackFunction f);
-   void setRightRotationHandler(CallbackFunction f);
-   void setLeftRotationHandler(CallbackFunction f);
-   void setUpperOverflowHandler(CallbackFunction f);
-   void setLowerOverflowHandler(CallbackFunction f);
+    void setChangedHandler(CallbackFunction f);
+    void setRightRotationHandler(CallbackFunction f);
+    void setLeftRotationHandler(CallbackFunction f);
+    void setUpperOverflowHandler(CallbackFunction f);
+    void setLowerOverflowHandler(CallbackFunction f);
 
-   int getID() const;
-   void setID(int newID);
+    int getID() const;
+    void setID(int newID);
 
-   bool operator == (ESPRotary &rhs);
+    bool operator == (ESPRotary &rhs);
 
-   void loop();
+    void loop();
 
- private:
-   static int _nextID;
+  private:
+    static int _nextID;
 
-   void _setID();
-
+    void _setID();
 };
 
 /////////////////////////////////////////////////////////////////

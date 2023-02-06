469a40baac0e609d29f970f853f76c8eba0a2972
Lennart Hennigs <mail@lennarthennigs.de>
2023-02-06 09:47:49 +0100

    updated formatting

--- src/ESPRotary.cpp
+++ src/ESPRotary.cpp
@@ -27,14 +27,14 @@ ESPRotary::ESPRotary() {
 
 /////////////////////////////////////////////////////////////////
 
-ESPRotary::ESPRotary(byte pin1, byte pin2, byte steps_per_click /* = 1 */,  int lower_bound /* = INT16_MIN */, int upper_bound /* = INT16_MAX */, int inital_pos /* = 0 */, int increment /* = 1 */) {
+ESPRotary::ESPRotary(byte pin1, byte pin2, byte steps_per_click /* = 1 */, int lower_bound /* = INT16_MIN */, int upper_bound /* = INT16_MAX */, int inital_pos /* = 0 */, int increment /* = 1 */) {
   ESPRotary();
   begin(pin1, pin2, steps_per_click, lower_bound, upper_bound, inital_pos, increment);
 }
 
 /////////////////////////////////////////////////////////////////
 
-void ESPRotary::begin(byte pin1, byte pin2, byte steps_per_click /* = 1 */,  int lower_bound /* = INT16_MIN */, int upper_bound /* = INT16_MAX */, int inital_pos /* = 0 */, int increment /* = 1 */) {
+void ESPRotary::begin(byte pin1, byte pin2, byte steps_per_click /* = 1 */, int lower_bound /* = INT16_MIN */, int upper_bound /* = INT16_MAX */, int inital_pos /* = 0 */, int increment /* = 1 */) {
   this->pin1 = pin1;
   this->pin2 = pin2;
   pinMode(pin1, INPUT_PULLUP);
@@ -44,7 +44,7 @@ void ESPRotary::begin(byte pin1, byte pin2, byte steps_per_click /* = 1 */,  int
   setLowerBound(lower_bound);
   setIncrement(increment);
   setStepsPerClick(steps_per_click);
-  
+
   loop();
   steps = inital_pos * steps_per_click;
   last_event = none;
@@ -54,7 +54,7 @@ void ESPRotary::begin(byte pin1, byte pin2, byte steps_per_click /* = 1 */,  int
 /////////////////////////////////////////////////////////////////
 
 void ESPRotary::setUpperBound(int upper) {
-  upper_bound = (lower_bound < upper) ? upper: lower_bound;
+  upper_bound = (lower_bound < upper) ? upper : lower_bound;
 }
 
 /////////////////////////////////////////////////////////////////
@@ -125,7 +125,7 @@ void ESPRotary::resetPosition(int p /* = 0 */, bool fireCallback /* = true */) {
   // yes...
   steps = p * steps_per_click;
   _isWithinBounds();
-  if (fireCallback && change_cb != NULL) change_cb (*this);
+  if (fireCallback && change_cb != NULL) change_cb(*this);
   last_event = none;
   dir = undefined;
   in_speedup = false;
@@ -187,7 +187,7 @@ void ESPRotary::setID(int newID) {
 
 /////////////////////////////////////////////////////////////////
 
-bool ESPRotary::operator == (ESPRotary &rhs) {
+bool ESPRotary::operator==(ESPRotary &rhs) {
   return (this == &rhs);
 }
 
@@ -202,10 +202,10 @@ void ESPRotary::loop() {
   if (enable_speedup) _checkForSpeedup(now);
   // are we out of bounds?
   if (_isWithinBounds(true)) {
-    // trigger rotation event  
+    // trigger rotation event
     _setEvent((dir == right) ? right_rotation : left_rotation);
     last_turn = now;
-    }
+  }
   last_steps = steps;
 }
 
@@ -216,16 +216,30 @@ bool ESPRotary::_wasRotated() {
   if (digitalRead(pin1)) s |= 4;
   if (digitalRead(pin2)) s |= 8;
   switch (s) {
-    case 0: case 5: case 10: case 15:
+    case 0:
+    case 5:
+    case 10:
+    case 15:
       break;
-    case 1: case 7: case 8: case 14:
-        steps += increment; break;
-    case 2: case 4: case 11: case 13:
-      steps -= increment; break;
-    case 3: case 12:
-      steps += 2 * increment; break;
+    case 1:
+    case 7:
+    case 8:
+    case 14:
+      steps += increment;
+      break;
+    case 2:
+    case 4:
+    case 11:
+    case 13:
+      steps -= increment;
+      break;
+    case 3:
+    case 12:
+      steps += 2 * increment;
+      break;
     default:
-      steps -= 2 * increment; break;
+      steps -= 2 * increment;
+      break;
   }
   state = (s >> 2);
   return (abs(steps - last_steps) >= steps_per_click * increment);
@@ -259,7 +273,7 @@ bool ESPRotary::_isWithinBounds(bool alert /* = false */) {
 
   } else if (pos <= lower_bound) {
     if (in_speedup) _setEvent(speedup_ended);
-    if (alert)  _setEvent(lower_bound_hit);
+    if (alert) _setEvent(lower_bound_hit);
     steps = lower_bound * steps_per_click;
   }
   return false;
@@ -269,81 +283,81 @@ bool ESPRotary::_isWithinBounds(bool alert /* = false */) {
 void ESPRotary::_setEvent(rotary_event e) {
   switch (e) {
     case left_rotation:
-      if (left_cb != NULL) left_cb (*this);
-      if (change_cb != NULL) change_cb (*this);
-    break;
+      if (left_cb != NULL) left_cb(*this);
+      if (change_cb != NULL) change_cb(*this);
+      break;
 
     case right_rotation:
-      if (right_cb != NULL) right_cb (*this);
-      if (change_cb != NULL) change_cb (*this);
-    break;
+      if (right_cb != NULL) right_cb(*this);
+      if (change_cb != NULL) change_cb(*this);
+      break;
 
     case speedup_started:
-      if (speedup_start_cb != NULL) speedup_start_cb (*this);
+      if (speedup_start_cb != NULL) speedup_start_cb(*this);
       in_speedup = true;
-    break;
+      break;
 
     case speedup_ended:
-      if (speedup_end_cb != NULL) speedup_end_cb (*this);
+      if (speedup_end_cb != NULL) speedup_end_cb(*this);
       in_speedup = false;
-    break;
+      break;
 
     case upper_bound_hit:
       if (last_event == upper_bound_hit && !retrigger_event) return;
-      if (upper_cb != NULL) upper_cb (*this);
-    break;
+      if (upper_cb != NULL) upper_cb(*this);
+      break;
 
     case lower_bound_hit:
       if (last_event == lower_bound_hit && !retrigger_event) return;
-      if (lower_cb != NULL) lower_cb (*this);
-    break;
+      if (lower_cb != NULL) lower_cb(*this);
+      break;
 
     case none:
-    break;
+      break;
   }
   last_event = e;
 }
 /////////////////////////////////////////////////////////////////
 
-  void ESPRotary::setSpeedupInterval(int time) {
-    speedup_interval = time;
-  }
+void ESPRotary::setSpeedupInterval(int time) {
+  speedup_interval = time;
+}
 
 /////////////////////////////////////////////////////////////////
 
-  int ESPRotary::getSpeedupInterval() const {
-    return speedup_interval;
-  }
+int ESPRotary::getSpeedupInterval() const {
+  return speedup_interval;
+}
 
 /////////////////////////////////////////////////////////////////
 
-  void ESPRotary::setSpeedupIncrement(int inc) {
-    speedup_increment = inc;
-  }
+void ESPRotary::setSpeedupIncrement(int inc) {
+  speedup_increment = inc;
+}
 
 /////////////////////////////////////////////////////////////////
 
-  int ESPRotary::getSpeedupIncrement() const {
-    return speedup_increment;
-  }
+int ESPRotary::getSpeedupIncrement() const {
+  return speedup_increment;
+}
 
 /////////////////////////////////////////////////////////////////
 
-  void ESPRotary::enableSpeedup(bool enable) {
-    enable_speedup = enable;
-  }
+void ESPRotary::enableSpeedup(bool enable) {
+  enable_speedup = enable;
+}
 
 /////////////////////////////////////////////////////////////////
 
-  bool ESPRotary::isSpeedupEnabled() const {
-    return enable_speedup;
-  }
+bool ESPRotary::isSpeedupEnabled() const {
+  return enable_speedup;
+}
 
 /////////////////////////////////////////////////////////////////
 
-  rotary_event ESPRotary::getLastEvent() const {
-    return last_event;
-  }
+rotary_event ESPRotary::getLastEvent() const {
+  return last_event;
+}
 
 /////////////////////////////////////////////////////////////////
 
--- src/ESPRotary.h
+++ src/ESPRotary.h
@@ -34,94 +34,94 @@ enum rotary_event {
 /////////////////////////////////////////////////////////////////
 
 class ESPRotary {
-  protected:
-    int id;
-    byte pin1, pin2;
-    byte steps_per_click;
-    int lower_bound;
-    int upper_bound;
-    byte state;
-    int increment;
-    int steps = 0;  
-    int last_steps = 0;
-    rotary_event last_event;
-    rotary_direction dir;
-    
-    bool retrigger_event = true;
-    bool enable_speedup = false;
-    int speedup_increment = 5;
-    int speedup_interval = 75;
-    int in_speedup = false;
-    long last_turn = 0;
+ protected:
+  int id;
+  byte pin1, pin2;
+  byte steps_per_click;
+  int lower_bound;
+  int upper_bound;
+  byte state;
+  int increment;
+  int steps = 0;
+  int last_steps = 0;
+  rotary_event last_event;
+  rotary_direction dir;
 
-    typedef void (*CallbackFunction) (ESPRotary&);
-    CallbackFunction change_cb = NULL;
-    CallbackFunction right_cb = NULL;
-    CallbackFunction left_cb = NULL;
-    CallbackFunction lower_cb = NULL;
-    CallbackFunction upper_cb = NULL;
+  bool retrigger_event = true;
+  bool enable_speedup = false;
+  int speedup_increment = 5;
+  int speedup_interval = 75;
+  int in_speedup = false;
+  long last_turn = 0;
 
-    CallbackFunction speedup_start_cb = NULL;
-    CallbackFunction speedup_end_cb = NULL;
+  typedef void (*CallbackFunction)(ESPRotary &);
+  CallbackFunction change_cb = NULL;
+  CallbackFunction right_cb = NULL;
+  CallbackFunction left_cb = NULL;
+  CallbackFunction lower_cb = NULL;
+  CallbackFunction upper_cb = NULL;
 
-  public:
-    ESPRotary();
-    ESPRotary(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
+  CallbackFunction speedup_start_cb = NULL;
+  CallbackFunction speedup_end_cb = NULL;
 
-    void begin(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
+ public:
+  ESPRotary();
+  ESPRotary(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
 
-    int getPosition() const;
-    void resetPosition(int p = 0, bool fireCallback = true);
+  void begin(byte pin1, byte pin2, byte steps_per_click = 1, int lower_bound = INT16_MIN, int upper_bound = INT16_MAX, int inital_pos = 0, int increment = 1);
 
-    rotary_direction getDirection() const;
-    String directionToString(rotary_direction dir) const;
+  int getPosition() const;
+  void resetPosition(int p = 0, bool fireCallback = true);
 
-    void setIncrement(int inc);
-    int getIncrement() const;
+  rotary_direction getDirection() const;
+  String directionToString(rotary_direction dir) const;
 
-    void enableSpeedup(bool enable);
-    void setSpeedupInterval(int time);
-    void setSpeedupIncrement(int inc);
+  void setIncrement(int inc);
+  int getIncrement() const;
 
-    bool isSpeedupEnabled() const;
-    int getSpeedupInterval() const;
-    int getSpeedupIncrement() const;
-    bool isInSpeedup() const;
+  void enableSpeedup(bool enable);
+  void setSpeedupInterval(int time);
+  void setSpeedupIncrement(int inc);
 
-    rotary_event getLastEvent() const;
-    void retriggerEvent(bool retrigger);
+  bool isSpeedupEnabled() const;
+  int getSpeedupInterval() const;
+  int getSpeedupIncrement() const;
+  bool isInSpeedup() const;
 
-    void setUpperBound(int upper_bound);
-    void setLowerBound(int lower_bound);
-    int getUpperBound() const;
-    int getLowerBound() const;
+  rotary_event getLastEvent() const;
+  void retriggerEvent(bool retrigger);
 
-    void setStepsPerClick(int steps);
-    int getStepsPerClick() const;
-   
-    void setChangedHandler(CallbackFunction f);
-    void setRightRotationHandler(CallbackFunction f);
-    void setLeftRotationHandler(CallbackFunction f);
-    void setUpperOverflowHandler(CallbackFunction f);
-    void setLowerOverflowHandler(CallbackFunction f);
-    void setSpeedupStartedHandler(CallbackFunction f);
-    void setSpeedupEndedHandler(CallbackFunction f);
+  void setUpperBound(int upper_bound);
+  void setLowerBound(int lower_bound);
+  int getUpperBound() const;
+  int getLowerBound() const;
 
-    int getID() const;
-    void setID(int newID);
+  void setStepsPerClick(int steps);
+  int getStepsPerClick() const;
 
-    bool operator == (ESPRotary &rhs);
+  void setChangedHandler(CallbackFunction f);
+  void setRightRotationHandler(CallbackFunction f);
+  void setLeftRotationHandler(CallbackFunction f);
+  void setUpperOverflowHandler(CallbackFunction f);
+  void setLowerOverflowHandler(CallbackFunction f);
+  void setSpeedupStartedHandler(CallbackFunction f);
+  void setSpeedupEndedHandler(CallbackFunction f);
 
-    void loop();
+  int getID() const;
+  void setID(int newID);
 
-  private:
-    static int _nextID;
-    
-    void _setEvent(rotary_event e);
-    bool _wasRotated();
-    bool _isWithinBounds(bool alert = false);
-    void _checkForSpeedup(long now);
-    void _setID();
+  bool operator==(ESPRotary &rhs);
+
+  void loop();
+
+ private:
+  static int _nextID;
+
+  void _setEvent(rotary_event e);
+  bool _wasRotated();
+  bool _isWithinBounds(bool alert = false);
+  void _checkForSpeedup(long now);
+  void _setID();
 };
 
 /////////////////////////////////////////////////////////////////
